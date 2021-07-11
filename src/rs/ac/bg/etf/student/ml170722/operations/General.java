559da5c9463675103904f79fca9570362394f381
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2021-07-11 20:39:36 +0200

    makes call to SQL stored procedures to empty db

--- src/rs/ac/bg/etf/student/ml170722/operations/General.java
+++ src/rs/ac/bg/etf/student/ml170722/operations/General.java
@@ -1,7 +1,6 @@
 package rs.ac.bg.etf.student.ml170722.operations;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
 import java.sql.SQLException;
 
 import rs.ac.bg.etf.student.ml170722.DB;
@@ -11,56 +10,13 @@ public class General implements GeneralOperations {
 
 	@Override
 	public void eraseAll() {
-		Connection connection;
-		try {
-			connection = DB.getInstance().getConnection();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			System.err.println("Couldn't connect to database");
-			return;
-		}
 		
-		{
-			// delete content of City table
-			String query = "DELETE FROM City";
-			try (PreparedStatement statement = connection.prepareStatement(query)) {
-
-				statement.executeUpdate();
-
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			// cascades to District table
-		}
-		
-		{
-			// delete content of Vehicle table
-			String query = "DELETE FROM Vehicle";
-			try (PreparedStatement statement = connection.prepareStatement(query)){
-				
-				statement.executeUpdate();
-			
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			// cascades to Courier table
-		}
-		
-		{
-			// delete content of dbUser table
-			String query = "DELETE FROM dbUser";
-			try (PreparedStatement statement = connection.prepareStatement(query)){
-				
-				statement.executeUpdate();
-			
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			// cascades to Courier table 
-			// cascades to Admin table 
+		String query = "{call sp_emptyDB}";
+		try (CallableStatement statement = DB.getInstance().getConnection().prepareCall(query)) {
+			statement.execute();
+		} catch (SQLException e) {
+//			e.printStackTrace();
+			System.err.println(e.getMessage());
 		}
 		
 	}
